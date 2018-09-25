d97369d618329dbfa68b5784e2c15f05cdb56430
Maksymilian <mkozak781@gmail.com>
2018-09-25 02:08:34 +0200

    fixed lightbox

--- js/modules/zoom.js
+++ js/modules/zoom.js
@@ -1,11 +1,29 @@
 function zoomGallery() {
     var zoom = document.querySelectorAll('.zoom');
 
+    var lightbox = document.getElementById("lightbox");
+ 
     zoom.forEach((el) => {
-        el.addEventListener('click', function handleClick(event) {
-            event.target.classList.add('active')
-            el.removeEventListener('click', handleClick);
-        });
-});
+        el.addEventListener('click', zoomIn);
+    })
 
+   
+    function zoomIn(event) {
+        event.target.classList.add('zoom__active');
+        lightbox.classList.add('block');
+    };
+
+
+    lightbox.onclick = function(){
+        lightbox.classList.remove('block');
+
+        let x = document.getElementsByClassName("zoom__active");
+
+        if(x.length > 0){
+            // Removing a class with VanillaJS
+            x[0].classList.remove("zoom__active");
+        }
+
+    }
 };
+
